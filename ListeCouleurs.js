2eaf2dc6e83645e27b55288856bbf8e9b6afaacb
PierreLamouroux <pierre.lamouroux22@gmail.com>
2019-11-20 10:25:31 +0100

    ListeCouleurs.js
    Liste JS pour les couleurs

---
+++ ListeCouleurs.js
@@ -0,0 +1,54 @@
+var rouge = new Object();
+rouge.nom = "rouge";
+rouge.code = "#FF0000";
+rouge.rbg = "RGB(255,0,0)";
+
+var rose = new Object();
+rose.nom = "rose";
+rose.code = "#FFC0CB";
+rose.rbg = "RGB(255,192,203)";
+
+var jaune = new Object(); 
+jaune.nom = "jaune";
+jaune.code = "#FFFF00";
+jaune.rbg = "RGB(255,255,0)";
+
+var violet = new Object(); 
+violet.nom = "violet";
+violet.code = "#800080";
+violet.rbg = "RGB(128,0,128)";
+
+var indigo = new Object();
+indigo.nom = "indigo";
+indigo.code = "#4B0082";
+indigo.rbg = "RGB(75,0,130)";
+
+var vert = new Object();
+vert.nom = "vert";
+vert.code = "#008000";
+vert.rbg = "RGB(0,128,0)";
+
+var bleu = new Object(); 
+bleu.nom = "bleu";
+bleu.code = "#0000FF";
+bleu.rbg = "RGB(0,0,255)";
+ 
+var marron = new Object();
+marron.nom = "marron";
+marron.code = "#A52A2A";
+marron.rbg = "RGB(165,42,42)";
+
+var blanc = new Object();
+blanc.nom = "blanc";
+blanc.code = "#FFFFFF";
+blanc.rbg = "RGB(255,255,255)";
+
+var gris = new Object();
+gris.nom = "gris";
+gris.code = "#808080";
+gris.rbg = "RGB(128,128,128)";
+
+var noir = new Object();
+noir.nom = "noir";
+noir.code = "#000000";
+noir.rbg = "RGB(0,0,0)";
